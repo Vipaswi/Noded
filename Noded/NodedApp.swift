e8d4ea7d2e45b1a2f80cb1e9708078611f8c66c5
vt7637@g.rit.edu <vt7637@g.rit.edu>
2026-02-10 21:07:40 -0500

    Initial Commit

---
+++ Noded/NodedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodedApp.swift
+//  Noded
+//
+//  Created by Vipaswi Jung Thapa on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct NodedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NodedTests/NodedTests.swift
@@ -0,0 +1,17 @@
+//
+//  NodedTests.swift
+//  NodedTests
+//
+//  Created by Vipaswi Jung Thapa on 2/10/26.
+//
+
+import Testing
+@testable import Noded
+
+struct NodedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
