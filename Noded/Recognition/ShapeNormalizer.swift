912572a4d8ade2eb768e672cd14d835a2743d0a1
Vipaswi <vipa.thapa@gmail.com>
2026-03-29 10:01:30 -0400

    Add Scaling and Translation functionality to shape normalizer.

--- Noded/Recognition/ShapeNormalizer.swift
+++ Noded/Recognition/ShapeNormalizer.swift
@@ -27,10 +27,58 @@ class ShapeNormalizer{
     // Find the indicative angle w from the point's centroid to the first point
     static func indicativeAngle(stroke: Stroke) -> Double {
         let centroid = calculateCentroid(stroke: stroke)
-        return atan2(firstPoint.y - centroid.y, firstPoint.x - centroid.x)
+        return atan2(stroke.strokePoints.first!.point.y - centroid.y, stroke.strokePoints.first!.point.x - centroid.x)
     }
 
     // Rotates a stroke by a certain angle
-    static func rotate 
+    static func rotateBy(stroke: Stroke, angle: Double) -> Stroke{
+        let centroid = calculateCentroid(stroke: stroke)
+        var rotatedPoints: [StrokePoint] = []
+
+        for strokePoint in stroke.strokePoints {
+            let translatedX = strokePoint.point.x - centroid.x
+            let translatedY = strokePoint.point.y - centroid.y
+
+            let rotatedX = translatedX * cos(angle) - translatedY * sin(angle)
+            let rotatedY = translatedX * sin(angle) + translatedY * cos(angle)
+
+            let newPoint = Point2D(x: rotatedX + centroid.x, y: rotatedY + centroid.y)
+            rotatedPoints.append(StrokePoint(point: newPoint, time: strokePoint.timestamp))
+        }
+
+        return Stroke(id: stroke.id, strokePoints: rotatedPoints)
+    }
+
+    static func scaleTo(stroke: Stroke, size: Double) -> Stroke {
+        let boundingBox = stroke.boundingBox
+        let scaleX = size / boundingBox.width
+        let scaleY = size / boundingBox.height
+        var scaledPoints: [StrokePoint] = []
+
+        for strokePoint in stroke.strokePoints {
+            let scaledX = (strokePoint.point.x - boundingBox.origin.x) * scaleX
+            let scaledY = (strokePoint.point.y - boundingBox.origin.y) * scaleY
+            let newPoint = Point2D(x: scaledX, y: scaledY)
+            scaledPoints.append(StrokePoint(point: newPoint, time: strokePoint.timestamp))
+        }
+
+        return Stroke(id: stroke.id, strokePoints: scaledPoints)
+    }
+
+    static func translateTo(stroke: Stroke, target: Point2D) -> Stroke {
+        let centroid = calculateCentroid(stroke: stroke)
+        let deltaX = target.x - centroid.x
+        let deltaY = target.y - centroid.y
+        var translatedPoints: [StrokePoint] = []
+
+        for strokePoint in stroke.strokePoints {
+            let translatedX = strokePoint.point.x + deltaX
+            let translatedY = strokePoint.point.y + deltaY
+            let newPoint = Point2D(x: translatedX, y: translatedY)
+            translatedPoints.append(StrokePoint(point: newPoint, time: strokePoint.timestamp))
+        }
+
+        return Stroke(id: stroke.id, strokePoints: translatedPoints)
+    }
 
 }
